b298fe28e400a50ecd3b9ad5b05b4ebe0e386553
Ali Yurtseven <58915125+aliyurtsevenn@users.noreply.github.com>
2022-03-01 15:49:32 +0300

    Update heatmap.py

--- covid_plasma_proteomics/Supplementary2/heatmap.py
+++ covid_plasma_proteomics/Supplementary2/heatmap.py
@@ -50,38 +50,13 @@ print(myPSM_filter_df)
 
 
 searcher_p="/home/ali/Desktop/results_covid_18_1_22_new_healthy/Figure2/Figure2C/critical_vs_healthy/critical_healthy.xlsx"
-# second_p="/home/a/Desktop/covid_project_3_July/critical_severe/critical_severe.xlsx"
-# searche="/home/a/Desktop/regulation/critical/upregulated_ones.xlsx"
-# searcher_p="/home/a/Desktop/critical.xlsx"
+
 search=pd.read_excel(searcher_p)
-# second=pd.read_excel(second_p)
-
-# search=search[(search["p_values"]<=0.01)]
-
-
-# second=second[(second["log2(Mean(First/Second))"]>0.75)|(second["log2(Mean(First/Second))"]<-0.75)]
-
-# second=second[second["p_values"]<0.04]
-# search=search[(search["Accession"].isin(second["Accession"]))]
-#
-# search=search[(search["Accession"]!="P00738")&(search["Accession"]!="P0C0L5")]
-
-# sec=pd.read_excel(second_p)
-#print(search)
-# cuttoff5=["VWF","S100A9","QSOX1","IHH","IGKV4-1","IGHV5-10-1","IGHV3-20","IGHV2-70D","IGFBP2","F9","CD163"]
-# cuttoff4=['ITIH3','IGHD','IGKV1-27','IGKV4-1','IGFBP2','IGHV3-20','VWF','CST3','S100A9','IGHV2-70D','PKM',
-#           'MB','IHH','AZGP1','F9','IGHV1-24']
-#
-# print(myPSM_filter_df)
-# sec=sec[(sec["log2(Mean(First/Second))"]>0.75)|(sec["log2(Mean(First/Second))"]<-0.75)]
 
 myPSM_filter_df=myPSM_filter_df[(myPSM_filter_df["Accession"].isin(search["Accession"]))]
 myPSM_filter_df.reset_index(inplace=True)
 myPSM_filter_df=myPSM_filter_df[myPSM_filter_df.columns[1:]]
-print(myPSM_filter_df)
-# sec.to_excel("/home/a/Desktop/nene.xlsx")
-# print(len(myPSM_filter_df))
-#
+
 avg=(myPSM_filter_df["H1"]+myPSM_filter_df["H2"]+myPSM_filter_df["H3"]+myPSM_filter_df["H4"]+myPSM_filter_df["H5"])/5
 
 myPSM_filter_df["M1_1"]=myPSM_filter_df["M1_1"]/avg
@@ -104,7 +79,6 @@ myPSM_filter_df["S3_2"]=myPSM_filter_df["S3_2"]/avg
 #
 myPSM_filter_df["C1_1"]=myPSM_filter_df["C1_1"]/avg
 myPSM_filter_df["C1_2"]=myPSM_filter_df["C1_2"]/avg
-# myPSM_filter_df["C1_3"]=myPSM_filter_df["C1_3"]/avg
 myPSM_filter_df["C2_1"]=myPSM_filter_df["C2_1"]/avg
 myPSM_filter_df["C2_2"]=myPSM_filter_df["C2_2"]/avg
 myPSM_filter_df["C2_3"]=myPSM_filter_df["C2_3"]/avg
@@ -117,26 +91,11 @@ myPSM_filter_df["C5_2"]=myPSM_filter_df["C5_2"]/avg
 myPSM_filter_df["C6_1"]=myPSM_filter_df["C6_1"]/avg
 myPSM_filter_df["C6_2"]=myPSM_filter_df["C6_2"]/avg
 
-# myPSM_filter_df["V1"]=myPSM_filter_df["V1"]/avg
-# myPSM_filter_df["V2"]=myPSM_filter_df["V2"]/avg
-# myPSM_filter_df["V3"]=myPSM_filter_df["V3"]/avg
-# myPSM_filter_df["V4"]=myPSM_filter_df["V4"]/avg
-#
 genes=myPSM_filter_df["genes_1"].tolist()
 
 zz= myPSM_filter_df[myPSM_filter_df.columns[8:]]
 
-print(zz)
 zz=np.log2(zz)
-print(search)
-print(zz)
-
-# genes= zz["genes"].tolist()
-
-# zz=zz[zz.columns[0:-2]]
-print(zz)
-
-
 
 
 pallete=sns.color_palette()
@@ -145,25 +104,13 @@ color_dict={}
 color_dict2={}
 color_dict3={}
 
-# for col in zz.columns:
-#     if col=="SI_1" or col=="SI_2" or col=="SI_3"or col=="SI_4"or col=="SI_5"or col=="SI_6":
-#         color_dict3[col]="darkturquoise"
-#     elif col=="SI_1" or col=="SI_2" or col=="SI_3"or col=="SI_4" or col=="SI_5"or col=="SI_6" or col=="SI_7"or col=="SI_8":
-#         color_dict3[col]="orange"
-#     else:
-#         color_dict3[col]="brown"
-
-# #
 for col in zz.columns:
-    # if  col=="H1" or col=="H2" or col=="H3":
-    #     color_dict2[col]="lightgreen"
-    if col=="M1_1" or col=="M1_2" or col=="M1_3"or col=="M2_1"or col=="M3_1"or col=="M4_1"or col=="M4_2":
+
+  if col=="M1_1" or col=="M1_2" or col=="M1_3"or col=="M2_1"or col=="M3_1"or col=="M4_1"or col=="M4_2":
         color_dict2[col]="darkturquoise"
     elif col=="S1_1" or col=="S1_2" or col=="S1_3"or col=="S2_1" or col=="S2_2"or col=="S2_3" or \
             col=="S3_1"or col=="S3_2":
         color_dict2[col]="orange"
-    # elif col=="V1" or col=="V2" or col=="V3" or col=="V4":
-    #     color_dict2[col] = "green"
     else:
         color_dict2[col]="brown"
 
@@ -175,13 +122,11 @@ for col in zz.columns:
         color_dict[col] = "crimson"
 
 color_col=pd.Series(color_dict)
-print(color_col)
-print(color_dict2)
+
 color_col2= pd.Series(color_dict2)
 
 k= sns.clustermap(zz,method="complete",metric="cityblock",cmap="coolwarm",row_cluster=True,col_cluster=False,col_colors=[color_col2,color_col],yticklabels=genes,vmin=-6,vmax=6,xticklabels=False,colors_ratio=0.015,cbar_kws={"label":"log2(FC)"})#
 
-# k.cax.set_visible(False)
 
 
 
@@ -200,7 +145,6 @@ plt.subplots_adjust(top=1.24,right=0.5,wspace=0,bottom=0.02)
 k.cax.set_position([0.63,.22,0.02,0.15])
 
 my_color1=[pallete[0],(52, 205, 235)]
-# my_color2=[pallete[3],pallete[4],pallete[5],pallete[6]]
 
 handles=[Patch(facecolor="navy"),Patch(facecolor="crimson")]
 handles2=[Patch(facecolor="darkturquoise"),Patch(facecolor="orange"),Patch(facecolor="rosybrown")]
